9266051fa78eae851ccf0a8c44c2b30ed333d749
Emmanuel Aina <ainae06@gmail.com>
2021-12-11 12:42:16 +0100

    adjust intro images for different platforms

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,27 +1,70 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
 import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 import { darkTheme } from "../config/colors";
+import AppointmentCard from "../components/AppointmentCard";
+import { Ionicons } from "@expo/vector-icons";
 
 const theme = darkTheme;
 
 const Home = () => {
 	return (
+		// <View style={styles.container}>
+		// 	<Text style={{color: 'white'}}>
+		// 		Home Screen
+		// 	</Text>
+		// </View>
+
 		<View style={styles.container}>
-			<Text style={{color: 'white'}}>
-				Home Screen
-			</Text>
+			<View
+				style={{ paddingTop: 70, flexDirection: "row", alignItems: "center" }}
+			>
+				<View style={styles.avatarText}>
+					<Text style={{ color: "white", fontSize: 23, fontWeight:'bold' }}>
+						Welcome Emmanuel 👋{" "}
+					</Text>
+				</View>
+				<View style={styles.avatarImage}>
+					<Image
+						source={require("../assets/images/stck1.jpeg")}
+						style={{ width: 50, height: 50, borderRadius: 100 / 2 }}
+					/>
+				</View>
+			</View>
+
+			<View style={styles.addNew}>
+				<Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
+					Book a new appointment
+				</Text>
+				<TouchableOpacity>
+					<Ionicons color="white" name="add-circle-outline" size={50} />
+				</TouchableOpacity>
+			</View>
+
+			<ScrollView style={{marginBottom:120}}>
+				<AppointmentCard />
+				<AppointmentCard />
+			</ScrollView>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
 	container: {
-		flex:1,
-		justifyContent: 'center',
+		flex: 1,
+		backgroundColor: theme.generalBackground,
+	},
+	avatarText: {
+		marginLeft: 15,
+	},
+	avatarImage: {
+		marginLeft: 70,
+	},
+	addNew: {
+		marginVertical: 100,
 		alignItems: 'center',
-		backgroundColor:theme.generalBackground,
-	}
-})
+		justifyContent:'center',
+	},
+});
 
-export default Home
+export default Home;

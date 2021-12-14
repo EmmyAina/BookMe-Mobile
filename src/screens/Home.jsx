fb233444a700c3d654ce429dd3aded4cab61013f
Emmanuel Aina <ainae06@gmail.com>
2021-12-14 19:42:47 +0100

    add today schedule

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -19,6 +19,18 @@ import { LinearGradient } from "expo-linear-gradient";
 
 const theme = darkTheme;
 
+// const today = [
+// 	{
+// 		id: "qwwre-1232h-j112-sq122-uuide3",
+// 		name: "Zanga's Cutx",
+// 		category: "Men's Cut",
+// 		approvalStatus: false,
+// 		date: "26-Dec-2021",
+// 		time: "11:30",
+// 	},
+// ];
+
+const today = [];
 // const fakeData = [
 // 	{
 // 		id: "qwwre-1232h-j112-sq122-uuide3",
@@ -45,10 +57,11 @@ const theme = darkTheme;
 // 		time: "09:00",
 // 	},
 // ];
-const fakeData = [];
+// const fakeData = [];
 
-const Home = (props, {navigation}) => {
-	const appointment = fakeData.map((item, index) => {
+const Home = (props, { navigation }) => {
+	// Render appointmet data from API
+	const appointment = today.map((item, index) => {
 		return (
 			<AppointmentCard
 				name={item.name}
@@ -65,18 +78,21 @@ const Home = (props, {navigation}) => {
 			/>
 		);
 	});
+
 	return (
 		<View style={styles.container}>
 			<View
 				style={{ paddingTop: 70, flexDirection: "row", alignItems: "center" }}
 			>
 				<View style={styles.avatarText}>
-					<Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>
+					<Text style={{ color: "white", fontSize: 35, fontWeight: "bold" }}>
 						Hi Emmanuel 👋{" "}
 					</Text>
 				</View>
 				<View style={styles.avatarImage}>
-					<TouchableOpacity onPress={() => props.navigation.navigate('Profile')}>
+					<TouchableOpacity
+						onPress={() => props.navigation.navigate("Profile")}
+					>
 						<SvgComponent width="30" height="30" />
 					</TouchableOpacity>
 				</View>
@@ -92,25 +108,14 @@ const Home = (props, {navigation}) => {
 							color="white"
 							name="add-circle"
 							size={70}
-							style={{ marginLeft: 15, marginTop: 10 }}
+							style={{ marginLeft: 0, marginTop: 10 }}
 						/>
-						<Text
-							style={{
-								color: "white",
-								fontSize: 20,
-								marginLeft: 15,
-								marginTop: 15,
-								fontWeight: "800",
-							}}
-						>
-							Create New
-						</Text>
+						<Text style={styles.createNewText}>Create New</Text>
 						<Text
 							style={{
 								color: "lightgrey",
 								fontSize: 15,
-								marginLeft: 15,
-								marginTop: 0,
+								marginTop: 4,
 							}}
 						>
 							Book New Appointment
@@ -120,29 +125,27 @@ const Home = (props, {navigation}) => {
 			</View>
 
 			<View>
-				{fakeData.length != 0 ? (
-					<ScrollView
-						style={{ marginTop: 50, marginBottom: 400 }}
-						showsVerticalScrollIndicator={false}
-					>
-						{appointment}
-					</ScrollView>
+				{today.length != 0 ? (
+					<View style={{ height: 350 }}>
+						<Text style={styles.upcming}>Today</Text>
+						<ScrollView
+							style={{ marginTop: 20, marginBottom: 1 }}
+							showsVerticalScrollIndicator={true}
+						>
+							{appointment}
+						</ScrollView>
+						<TouchableOpacity
+							onPress={() => props.navigation.navigate("Schedule")}
+						>
+							<Text style={styles.viewAll}>View All</Text>
+						</TouchableOpacity>
+					</View>
 				) : (
 					<ScrollView
 						style={{ marginTop: 50, marginBottom: 100, height: 200 }}
 						showsVerticalScrollIndicator={false}
 					>
-						<Text
-							style={{
-								textAlign: "center",
-								fontWeight: "600",
-								fontSize: 30,
-								color: "#5f6470",
-								marginTop: 100,
-							}}
-						>
-							You have no Appointments!
-						</Text>
+						<Text style={styles.noApt}>You have no Appointments Today!</Text>
 					</ScrollView>
 				)}
 			</View>
@@ -156,29 +159,56 @@ const styles = StyleSheet.create({
 		backgroundColor: theme.generalBackground,
 	},
 	avatarText: {
-		flex: 0.8,
-		marginLeft: 15,
+		marginLeft: 20,
 	},
 	avatarImage: {
-		flex: 0.2,
-		marginRight: 20,
-		width: 60,
-		height: 60,
-		backgroundColor: "white",
+		marginLeft: 50,
+		width: 70,
+		height: 70,
+		backgroundColor: theme.formInput,
 		alignItems: "center",
 		justifyContent: "center",
 		borderRadius: 90 / 1,
-		borderWidth: 1,
-		borderColor: "lightgrey",
 	},
 	addNewContainer: {
+		// alignItems: "center",
 		marginTop: 30,
-		marginLeft: 15,
+		marginLeft: 20,
 	},
 	addNewButton: {
-		width: 200,
+		alignItems: "center",
+		width: 180,
 		height: 150,
 		borderRadius: 50 / 4,
+	},
+	createNewText: {
+		color: "white",
+		fontSize: 20,
+		marginLeft: 0,
+		marginTop: 15,
+		fontWeight: "800",
+	},
+	upcming: {
+		color: "white",
+		fontSize: 25,
+		fontWeight: "400",
+		// textAlign: "center",
+		marginTop: 50,
+		marginLeft: 20,
+	},
+	noApt: {
+		textAlign: "center",
+		fontWeight: "600",
+		fontSize: 30,
+		color: "#5f6470",
+		marginTop: 100,
+	},
+	viewAll: {
+		color: "white",
+		fontSize: 20,
+		fontWeight: "bold",
+		textAlign: "center",
+		// textDecorationLine: "underline",
 	},
 });
 
